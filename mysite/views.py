812dafbdc5625c09d7341a5738c784250f880fef
carrera95 <eacs_95@hotmail.com>
2024-11-21 12:35:00 -0600

    Update mysite view
    Add JWT Tokens to login and register methods

--- mysite/views.py
+++ mysite/views.py
@@ -1,29 +1,42 @@
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-from users.serializers import UserSerializer
-from rest_framework.authtoken.models import Token
 from rest_framework import status
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
+from rest_framework.permissions import AllowAny 
+from rest_framework_simplejwt.tokens import RefreshToken
+from .serializers import MyTokenObtainPairSerializer
+from users.serializers import UserSerializer
 from users.models import CustomUser
 from django.shortcuts import get_object_or_404
-from rest_framework.permissions import AllowAny
-from rest_framework.decorators import permission_classes
 
 @api_view(['POST'])
 @permission_classes([AllowAny])
-def login(request):
-    user = get_object_or_404(CustomUser, username=request.data['username'])
-    
-    if not user.check_password(request.data['password']):
-        return Response({
-            'Error': 'Invalid password'
-        },  status=status.HTTP_400_BAD_REQUEST)
-    token, created = Token.objects.get_or_create(user=user)
-    serializer = UserSerializer(instance=user)
+def login(request): 
+    user = get_object_or_404(
+        CustomUser, 
+        username=request.data['username']
+    )
 
-    return Response({
-        'Token': token.key,
-        'User': serializer.data
-    },  status=status.HTTP_200_OK)
+    if not user.check_password(request.data['password']): 
+        return Response({ 
+            'Error': 'Invalid password' 
+        }, status=status.HTTP_400_BAD_REQUEST)
+    
+    refresh = RefreshToken.for_user(user) 
+    access_token = str(refresh.access_token) 
+    refresh_token = str(refresh)
+    serializer = UserSerializer(instance=user) 
+    
+    return Response({ 
+        'access': access_token, 
+        'refresh': refresh_token, 
+        'user': { 
+            'id': user.id, 
+            'username': user.username, 
+            'email': user.email, 
+            'name': user.first_name, 
+            'is_admin': user.is_staff 
+        } 
+    }, status=status.HTTP_200_OK)
 
 @api_view(['POST'])
 @permission_classes([AllowAny])
@@ -31,17 +44,19 @@ def register(request):
     serializer = UserSerializer(data=request.data)
 
     if serializer.is_valid():
-        serializer.save()
-        user = CustomUser.objects.get(username=serializer.data['username'])
-        user.uncripted_pswd = request.data['password']
-        user.set_password(serializer.data['password'])
+        user = serializer.save() 
+        user.uncripted_pswd = request.data['password'] 
+        user.set_password(serializer.data['password']) 
         user.save()
 
-        token = Token.objects.create(user=user)
+        refresh = RefreshToken.for_user(user) 
+        access_token = str(refresh.access_token) 
+        refresh_token = str(refresh)
 
         return Response({
-            'Token': token.key,
-            'User': serializer.data
+            'access': access_token, 
+            'refresh': refresh_token, 
+            'user': serializer.data
         },status=status.HTTP_200_OK)
     
     return Response(
